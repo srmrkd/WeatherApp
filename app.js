1c8c2221cb5f0835757434ea09d2e22d40bce13d
Sara <saramrkd@gmail.com>
2024-04-03 01:40:51 +0200

    styling and adding API

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
-const tempratue = document.querySelector('.temprature');
-
-
-
 /* To run it all after the window loads */
 window.addEventListener('load', ()=>{
     /* Define the cordinates to find the geo of my current location */
-    let longitude;
     let latitude;
+    let longitude;
 
     /* navigator.geolocation is a built-in JavaScript object that provides access to the device's geolocation information, such as latitude and longitude coordinates. */
     if (navigator.geolocation) { /* Checks if the browser supports the Geolocalization */
         navigator.geolocation.getCurrentPosition(position =>{
-            longitude = position.coords.longitude;
             latitude = position.coords.latitude;
-            console.log(longitude, latitude);
+            longitude = position.coords.longitude;
+            console.log(latitude, longitude);
+            // API Key
+            const key = `84112e343d54954aac2caa417aa9d8ae`;
+            // Get Weather from API Provider
+            let api = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`;
+            async function checkWeather() {
+                fetch(api)
+                .then(response =>{
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    document.querySelector('.location-timezone').innerHTML = data.city.name;
+                    document.querySelector('.location img');
+                    document.querySelector('.temprature h2').innerHTML = data.list[0].main.temp;
+                    document.querySelector('.feelslike').innerHTML = data.list[0].main.feels_like;
+                    document.querySelector('.humidity').innerHTML = data.list[0].main.humidity;
+                    document.querySelector('.wind').innerHTML = data.list[0].wind.speed;
+                })
+            }
+            checkWeather();
         });
     } else {
         tempratue.style.display = 'block';
         tempratue.innerHTML = `<p> Browser does not support Geolocalization`;
     } 
-    
-    // API Key
-    const key = `84112e343d54954aac2caa417aa9d8ae`;
 
-    // Get Weather from API Provider
-    let api =`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
-
-    fetch(api)
-        .then(response =>{
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);  
-        })
 })
